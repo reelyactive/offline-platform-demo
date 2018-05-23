e7310d082261bf42d69953f8d304d040636a4f48
jeffyactive <jeff@reelyactive.com>
2018-05-22 21:20:01 -0400

    Added json-silo

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /**
- * Copyright reelyActive 2017
+ * Copyright reelyActive 2017-2018
  * We believe in an open Internet of Things
  */
 
@@ -7,12 +7,20 @@ const http = require('http');
 const express = require('express');
 const cors = require('cors');
 const hlcserver = require('hlc-server');
+const jsonsilo = require('json-silo');
+
+
+// ----- Begin user-editable parameters -----
+const PASSWORD = 'admin';
+// ----- End user-editable parameters -----
 
 
 const SNIFFYPEDIA_PORT = 3000;
 const HLC_SERVER_PORT = 3001;
+const JSON_SILO_PORT = 3002;
 const SNIFFYPEDIA_ROOT = 'http://localhost:' + SNIFFYPEDIA_PORT + '/';
 const HLC_SERVER_ROOT = 'http://localhost:' + HLC_SERVER_PORT + '/';
+const JSON_SILO_ROOT = 'http://localhost:' + JSON_SILO_PORT + '/';
 const UDP_LISTENER_PATH = '0.0.0.0:50000';
 
 
@@ -39,6 +47,9 @@ server.listen(SNIFFYPEDIA_PORT, function() {
 // Define the hlc-server options
 var options = {
     httpPort: HLC_SERVER_PORT,
+    protectedRequests: [
+        { path: "/associations/", methods: [ "DELETE" ] }
+    ],
     chickadee: {
         associationsRootUrl: SNIFFYPEDIA_ROOT
     }
@@ -56,12 +67,33 @@ hlc.bind( { protocol: 'udp', path: UDP_LISTENER_PATH } );
 console.log('hlc-server hosted on', HLC_SERVER_ROOT);
 
 
+// ----- JSON-SILO -----------------------------------------------------------
+
+// Define the json-silo options
+options = {
+    httpPort: JSON_SILO_PORT,
+    useCors: true,
+    stations: [],
+    directories: [],
+    durations: [ { title: "1 year", value: "365d" },
+                 { title: "1 week", value: "7d" },
+                 { title: "1 day", value: "24h" } ],
+    password: PASSWORD,
+    identifierLength: 4
+};
+
+// Create the json-silo using the above options
+var silo = new jsonsilo(options);
+
+
 // ----- CONSOLE INSTRUCTIONS ------------------------------------------------
 
 console.log('\r\n--------------------------------------' +
             '------------------------------------------');
 console.log('--> Browse to ' + HLC_SERVER_ROOT +
             ' for the reelyActive offline demo');
+console.log('--> Browse to ' + JSON_SILO_ROOT +
+            ' to onboard devices');
 console.log('------------------------------------------' +
             '--------------------------------------\r\n');
 
